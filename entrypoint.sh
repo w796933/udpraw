bc1a201abc66f51d53a4879a59affcae8e6d4b48
linyuhc001 <linyuhc001@gmail.com>
2017-11-28 23:45:49 +0800

    Update entrypoint.sh

--- entrypoint.sh
+++ entrypoint.sh
@@ -4,4 +4,4 @@ kcpserver -t 127.0.0.1:8989 -l :29900 -mode fast2 -mtu 1200 -datashard 0 -parity
 
 ss-server -s 0.0.0.0 -p 8989 -m chacha20 -k 131415 --fast-open &
 
-udpraw -s -l0.0.0.0:8855 -r 127.0.0.1:29900 -k "131415" --raw-mode faketcp -a
+udpraw -s -l0.0.0.0:8855 -r 127.0.0.1:29900 -k "131415" --lower-level auto --raw-mode faketcp -a
