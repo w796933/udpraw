adbfea7a66392f014143552b5766b98bcb192e1b
linyuhc001 <linyuhc001@gmail.com>
2017-12-22 13:46:09 +0800

    Update entrypoint.sh

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-ss-server -s 0.0.0.0 -p 8989 -m aes-256-gcm -k 131415 &
+ss-server -s 0.0.0.0 -p 8989 -m aes-256-gcm -k 131415 --fast-open &
 
 kcpserver -t 127.0.0.1:8989 -l :29900 -mode fast2 -mtu 1200 -datashard 0 -parityshard 0 -sndwnd 2048 -rcvwnd 1024 -nocomp true -quiet &
 
